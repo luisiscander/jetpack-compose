7b4b3c784be4cd79274702b79040723b6974b549
Luiso <luisiscander2022@gmail.com>
2024-09-11 23:16:36 -0500

    add preview, image,text, colum and Row

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -4,21 +4,72 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+setContent {
+    myComponent()
+}
 
     }
+
+    @Composable
+    fun image(){
+        Image(
+            painterResource(R.drawable.ic_launcher_foreground),
+            "testing image")
+
+    }
+
+    @Composable
+    fun myText(text:String)=Text(text)
+
+    @Composable
+    fun myTexts(){
+       Column {
+           myText("Hi Jetpack compose.!!")
+           myText("Ready.?")
+       }
+    }
+
+    @Composable
+    fun myComponent(){
+        Row {
+            image()
+            myTexts()
+        }
+    }
+
+    @Preview
+    @Composable
+    fun getPreview(){
+        myComponent()
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 
